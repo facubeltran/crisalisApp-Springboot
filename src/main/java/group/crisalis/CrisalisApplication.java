8396c63351271daa8119391063d5d09beedf70c3
facubeltran <facundocristianbeltran@gmail.com>
2022-12-14 12:41:50 -0300

    1er commit

---
+++ src/main/java/group/crisalis/CrisalisApplication.java
@@ -0,0 +1,13 @@
+package group.crisalis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrisalisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrisalisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group/crisalis/CrisalisApplicationTests.java
@@ -0,0 +1,13 @@
+package group.crisalis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrisalisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
