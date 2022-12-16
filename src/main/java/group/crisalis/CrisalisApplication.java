1d8a9b06e5827895c2a139d93d2b211f63cf2883
facubeltran <facundocristianbeltran@gmail.com>
2022-12-16 12:09:03 -0300

    corregi la conexion sqlserver

--- src/main/java/group/crisalis/CrisalisApplication.java
+++ src/main/java/group/crisalis/CrisalisApplication.java
@@ -3,6 +3,7 @@ package group.crisalis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CrisalisApplication {
 
